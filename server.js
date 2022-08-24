e8d4e375f7b8f6736c58501afca2f64c479b9ca2
Matheus Ferreira <alves.mfp@gmail.com>
2022-08-24 20:55:05 -0300

    Definindo nova porta

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app.js');
-const port = process.env.PORT || 3000;
+const port = 3001;
 
 app.listen(port, () => {
    console.log(`Servidor conectado em http://localhost:${port}`)
